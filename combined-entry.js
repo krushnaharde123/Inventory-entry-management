0ce966d4949d64ed6ddc2972cfb24baa9306bda2
krushnaharde123 <krushnaharde@outlook.com>
2025-02-26 23:37:33 +0530

    Update combined-entry.js

--- combined-entry.js
+++ combined-entry.js
@@ -1,11 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-<script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
+  
   // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
@@ -17,11 +14,6 @@ document.addEventListener('DOMContentLoaded', function () {
     appId: "1:863294594287:web:49b1e9567abe0939544f1a",
     measurementId: "G-E7H9J01X63"
   };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 
     // Initialize Firebase
     const app = firebase.initializeApp(firebaseConfig);
